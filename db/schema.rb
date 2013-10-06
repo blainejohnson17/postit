10dc7166997e5b3e7ea0bb71e014a557ed521a85
Blaine Johnson <blainejohnson17@gmail.com>
2013-10-05 22:55:08 -0600

    Adds Categorization model, Post controller, and Post resources

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131004183458) do
+ActiveRecord::Schema.define(version: 20131004185051) do
+
+  create_table "categories", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "categorizations", force: true do |t|
+    t.integer  "post_id"
+    t.integer  "category_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "comments", force: true do |t|
     t.integer  "user_id"
