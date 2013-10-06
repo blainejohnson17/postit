2f92924149ff67cf21afa2fdeb0d0e0ecaafd68b
Blaine Johnson <blainejohnson17@gmail.com>
2013-10-05 23:25:09 -0600

    adds all post resources actions to the PostController

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,29 +1,48 @@
 class PostsController < ApplicationController
   def index
-
+    @post = Post.all
   end
 
   def show
-
+    @post = Post.find(params[:id])
   end
 
   def new
-    
+    @post = Post.new
   end
 
   def create
+    post = Post.new(post_params)
 
+    if post.save
+      redirect_to posts_path, :notice 'Post was successfully created.'
+    else
+      render :new
+    end
   end
 
   def edit
-    
+    @post = Post.find(params[:id])
   end
 
   def update
-    
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to post_path(@post), :notice 'The post was succesfully updated.'
+    else
+      render :edit
+    end
   end
 
   def destroy
-
+    @post = Post.find(params[:id])
+    @post.destroy
+   
+    redirect_to posts_path
   end
+
+  private
+    def user_params
+      params.require(:post).permit(:title, :url, :description)
+    end
 end
