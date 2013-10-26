1c42aa32ec469d44d7f3e601a9ff42d0f4ee6a00
Blaine Johnson <blainejohnson17@gmail.com>
2013-10-26 14:04:19 -0600

    Adds user show template Modifies posts partial to just show single post

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  include Sluggable
+
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :votes, dependent: :destroy
@@ -7,4 +9,6 @@ class User < ActiveRecord::Base
 
   validates :username, presence: true, uniqueness: true
   validates :password, presence: true, on: :create, length: {minimum: 3}
+
+  sluggable_column :username
 end
